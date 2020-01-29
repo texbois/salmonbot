27b3f583f5a3c8a76cc18b8ace0a63b5e6f9d90e
timlathy <tim.labushev@gmail.com>
2020-01-29 09:48:35 +0300

    compute hamming distance between received images

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,30 @@ async fn main() -> BotResult<()> {
 async fn process_message(vk: &vkapi::VkApi, msg: vkapi::VkMessage) -> BotResult<()> {
     println!("Message: {:?}", msg);
 
-    for photo in msg.attachments {
-        let img_bytes = vk.fetch_photo(&photo).await?;
-        let img = image::load_from_memory(&img_bytes)?;
+    if msg.attachments.len() == 2 {
+        let image1 = image::load_from_memory(&vk.fetch_photo(&msg.attachments[0]).await?)?;
+        let image2 = image::load_from_memory(&vk.fetch_photo(&msg.attachments[1]).await?)?;
 
         let hasher = img_hash::HasherConfig::new()
             .hash_alg(img_hash::HashAlg::Mean)
             .preproc_dct()
             .to_hasher();
-        let hash = hasher.hash_image(&img);
 
-        println!("Image hash: {:?}", hash);
+        let hash1 = hasher.hash_image(&image1);
+        let hash2 = hasher.hash_image(&image2);
+
+        let distance = hamming::distance(hash1.as_bytes(), hash2.as_bytes());
+
+        let reply = format!(
+            "hash 1: {:?}, hash 2: {:?}, hamming distance: {}",
+            hash1.as_bytes(),
+            hash2.as_bytes(),
+            distance
+        );
+
+        vk.send_message(msg.from_id, &reply).await
+    } else {
+        vk.send_message(msg.from_id, "Two attachments required for hash comparison")
+            .await
     }
-    Ok(())
 }
