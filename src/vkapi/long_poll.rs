203beaa23c0b58739bb833fbc94c04f9f52d0868
timlathy <tim.labushev@gmail.com>
2020-02-02 23:41:16 +0300

    treat image documents as attachments

--- src/vkapi/long_poll.rs
+++ src/vkapi/long_poll.rs
@@ -1,5 +1,6 @@
 use crate::vkapi::{Client, VkApi};
 use serde_derive::Deserialize;
+use serde_json::Value as JsonValue;
 
 pub struct VkLongPoll<'a, C: Client> {
     pub state: VkLongPollState,
@@ -19,6 +20,7 @@ pub struct VkMessage {
     pub from_id: i64,
     pub attachments: Vec<VkPhoto>,
     pub forwarded: Vec<Box<VkMessage>>,
+    pub reply_to: Option<Box<VkMessage>>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -42,10 +44,10 @@ impl<'a, C: Client> VkLongPoll<'a, C> {
             ("ts", &self.state.ts),
             ("wait", "25"),
         ];
-        let mut resp: serde_json::Value =
-            self.api
-                .client
-                .get_json(&self.state.server, &params, None)?;
+        let mut resp: JsonValue = self
+            .api
+            .client
+            .get_json(&self.state.server, &params, None)?;
 
         if let Some(err) = resp.get("failed").and_then(|e| e.as_u64()) {
             if err < 4 {
@@ -56,70 +58,81 @@ impl<'a, C: Client> VkLongPoll<'a, C> {
             }
         } else {
             self.state.ts = match resp.get_mut("ts").map(|ts| ts.take()) {
-                Some(serde_json::Value::String(ts)) => ts,
+                Some(JsonValue::String(ts)) => ts,
                 _ => return Err(format!("Long poll response missing \"ts\": {:?}", resp).into()),
             };
-            match resp.get_mut("updates").map(|u| u.take()) {
-                Some(serde_json::Value::Array(updates)) => updates
-                    .into_iter()
+            if let Some(JsonValue::Array(updates)) = resp.get_mut("updates") {
+                updates
+                    .iter_mut()
                     .filter_map(try_parse_update)
-                    .try_for_each(&mut callback),
-                _ => Err(format!("Long poll response missing \"updates\": {:?}", resp).into()),
+                    .try_for_each(&mut callback)
+            } else {
+                Err(format!("Long poll response missing \"updates\": {:?}", resp).into())
             }
         }
     }
 }
 
-fn try_parse_update(mut update: serde_json::Value) -> Option<VkMessage> {
-    update
-        .get_mut("object")?
-        .as_object_mut()?
-        .remove("message")
-        .and_then(try_parse_message)
+fn try_parse_update(update: &mut JsonValue) -> Option<VkMessage> {
+    try_parse_message(update.get_mut("object")?.get_mut("message")?)
 }
 
-fn try_parse_message(mut message: serde_json::Value) -> Option<VkMessage> {
+fn try_parse_message(message: &mut JsonValue) -> Option<VkMessage> {
     let text = match message.get_mut("text").map(|t| t.take()) {
-        Some(serde_json::Value::String(s)) => s,
+        Some(JsonValue::String(s)) => s,
         _ => String::new(),
     };
     let from_id = message.get("from_id")?.as_i64()?;
-    let attachments = match message.get_mut("attachments").map(|a| a.take()) {
-        Some(serde_json::Value::Array(atts)) => atts
-            .into_iter()
-            .filter_map(try_extract_attachment)
-            .collect(),
-        _ => Vec::new(),
-    };
-    let forwarded = match message.get_mut("fwd_messages").map(|a| a.take()) {
-        Some(serde_json::Value::Array(msgs)) => msgs
-            .into_iter()
-            .filter_map(try_parse_message)
-            .map(Box::new)
-            .collect(),
-        _ => Vec::new(),
-    };
+    let attachments = message
+        .get_mut("attachments")
+        .and_then(|a| a.as_array_mut())
+        .map(|atts| atts.iter_mut().filter_map(try_extract_attachment).collect())
+        .unwrap_or(Vec::new());
+    let forwarded = message
+        .get_mut("fwd_messages")
+        .and_then(|a| a.as_array_mut())
+        .map(|atts| {
+            atts.iter_mut()
+                .filter_map(try_parse_message)
+                .map(Box::new)
+                .collect()
+        })
+        .unwrap_or(Vec::new());
+    let reply_to = message
+        .get_mut("reply_message")
+        .and_then(try_parse_message)
+        .map(Box::new);
+
     Some(VkMessage {
         text,
         from_id,
         attachments,
         forwarded,
+        reply_to,
     })
 }
 
-fn try_extract_attachment(mut attachment: serde_json::Value) -> Option<VkPhoto> {
-    let mut max_photo_size_url = attachment
+fn try_extract_attachment(attachment: &mut JsonValue) -> Option<VkPhoto> {
+    let photo_obj = if let Some(doc) = attachment.get_mut("doc") {
+        doc.get_mut("preview")?
+    } else {
+        attachment
+    };
+
+    let mut opt_size_obj = photo_obj
         .get_mut("photo")?
-        .as_object_mut()?
-        .remove("sizes")?
+        .get_mut("sizes")?
         .as_array_mut()?
-        .drain(0..)
+        .drain(..)
         .filter(|size| ["m", "x", "y", "z", "w"].contains(&size["type"].as_str().unwrap_or("")))
         .min_by_key(|size| size["width"].as_u64().unwrap_or(std::u64::MAX))?;
 
-    match max_photo_size_url["url"].take() {
-        serde_json::Value::String(max_size_url) => Some(VkPhoto { max_size_url }),
-        _ => None,
+    if let Some(JsonValue::String(url)) = opt_size_obj.get_mut("url").map(|u| u.take()) {
+        Some(VkPhoto { max_size_url: url })
+    } else if let Some(JsonValue::String(url)) = opt_size_obj.get_mut("src").map(|u| u.take()) {
+        Some(VkPhoto { max_size_url: url })
+    } else {
+        None
     }
 }
 
@@ -164,6 +177,48 @@ mod tests {
     }
 
     #[test]
+    fn test_parse_reply_document() {
+        let vk = VkApi {
+            client: crate::vkapi::http::TestClient::new("long_poll_reply_document.json"),
+            token: "token".into(),
+            community_name: "sample_community".into(),
+            community_id: "1001".into(),
+        };
+        let mut msg: Option<VkMessage> = None;
+        VkLongPoll {
+            api: &vk,
+            state: VkLongPollState {
+                key: "long_poll_key".into(),
+                server: "https://long_poll_server".into(),
+                ts: "100".into(),
+            },
+        }
+        .poll_once(|m| {
+            msg = Some(m);
+            Ok(())
+        })
+        .unwrap();
+        assert_eq!(
+            msg,
+            Some(VkMessage {
+                text: "but they are!".to_owned(),
+                from_id: 1010,
+                attachments: vec![],
+                forwarded: vec![],
+                reply_to: Some(Box::new(VkMessage {
+                    text: "uh, docs aren't photos...".into(),
+                    from_id: 1000,
+                    attachments: vec![VkPhoto {
+                        max_size_url: "$med_url".into()
+                    }],
+                    forwarded: vec![],
+                    reply_to: None
+                }))
+            })
+        );
+    }
+
+    #[test]
     fn test_parse_forwarded_attachment() {
         let vk = VkApi {
             client: crate::vkapi::http::TestClient::new("long_poll_fwd_attachment.json"),
@@ -188,17 +243,19 @@ mod tests {
         assert_eq!(
             msg,
             Some(VkMessage {
-                text: "hey check this out".to_owned(),
+                text: "hey check this out".into(),
                 from_id: 1010,
                 attachments: vec![],
                 forwarded: vec![Box::new(VkMessage {
-                    text: "forwarded text".to_owned(),
+                    text: "forwarded text".into(),
                     from_id: 1020,
                     attachments: vec![VkPhoto {
                         max_size_url: "$med_url".into()
                     }],
-                    forwarded: vec![]
-                })]
+                    forwarded: vec![],
+                    reply_to: None
+                })],
+                reply_to: None
             })
         );
     }
